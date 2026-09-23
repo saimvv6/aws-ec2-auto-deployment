03bd1cf1cb06df89078aeaaca8585b3a8284c405
saimvv6 <saimvv6@gmail.com>
2026-09-23 23:33:02 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,6 @@
 variable "ami_id" {
   description = "The AMI ID for the EC2 instance"
   type        = string
-  # Ikkada nuvvu AWS lo chusi oka valid Free-tier Ubuntu leda Amazon Linux AMI ID ivvu.
   # Example: "ami-0e86e20dae9224db8" (us-east-1 region ki)
   default     = "ami-0e86e20dae9224db8" 
 }
